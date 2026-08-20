fd72291d1d6f5655ce14b9a55019adff64c9f902
Cedric <cedricck123@gmail.com>
2026-08-21 03:32:54 +0800

    Completed level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,18 @@
 public class Duke {
     public static void main(String[] args) {
-        String banner = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String line = "____________________________________________________________";
+        String banner = "  ____                            _        _     \n"
+                + " / ___|___  _ __ ___  _ __  _   _| |_ __ _| |__  \n"
+                + "| |   / _ \\| '_ ` _ \\| '_ \\| | | | __/ _` | '_ \\ \n"
+                + "| |__| (_) | | | | | | |_) | |_| | || (_| | | | |\n"
+                + " \\____\\___/|_| |_| |_| .__/ \\__,_|\\__\\__,_|_| |_|\n"
+                + "                     |_|                          \n";
+        System.out.println(line);
         System.out.println(banner);
+        System.out.println("Hello! I'm Computah.");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
     }
 }
